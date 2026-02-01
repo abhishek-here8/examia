ec32c193d93c0014aa88d0b89675e548922904cb
Abhishek Jandu <janduabhishek8@gmail.com>
2026-02-01 10:37:56 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,38 @@
+window.EAMIA_QUESTIONS = {
+  physics: {
+    "2024": {
+      chapters: {
+        "Kinematics": [
+          { q: "A body moves with constant acceleration. Define it.", ans: "Rate of change of velocity." },
+          { q: "Write equation: v^2 - u^2 = ?", ans: "2as" }
+        ],
+        "NLM": [
+          { q: "State Newton’s 2nd law.", ans: "F = ma (rate of change of momentum)." }
+        ]
+      },
+      papers: {
+        "Paper 1": [
+          { q: "What is impulse?", ans: "Change in momentum." }
+        ],
+        "Paper 2": [
+          { q: "Define power.", ans: "Work done per unit time." }
+        ]
+      }
+    },
+
+    "2025": { chapters: {}, papers: {} },
+    "2026": { chapters: {}, papers: {} }
+  },
+
+  chemistry: {
+    "2024": { chapters: {}, papers: {} },
+    "2025": { chapters: {}, papers: {} },
+    "2026": { chapters: {}, papers: {} }
+  },
+
+  maths: {
+    "2024": { chapters: {}, papers: {} },
+    "2025": { chapters: {}, papers: {} },
+    "2026": { chapters: {}, papers: {} }
+  }
+};
